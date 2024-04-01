f65171c079c9b997cc0b6a9e786d300c5bd09cc7
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2024-04-01 15:21:27 +0200

    Refactor vecop.py

--- symmtools/vecop.py
+++ symmtools/vecop.py
@@ -1,48 +1,99 @@
-__all__ = ('vector', 'normalize', 'same', 'parallel', 'perpendicular',
-           'translate', 'invert', 'translate2', 'rotate', 'reflect')
+"""Functions for basic vector operations."""
 
-from numpy import array, sin, cos, dot, cross
+__all__ = (
+    "Vector",
+    "vector",
+    "normalize",
+    "same",
+    "parallel",
+    "perpendicular",
+    "translate",
+    "invert",
+    "translate2",
+    "rotate",
+    "reflect",
+)
+
+from collections.abc import Sequence
+from typing import Union
+
+from numpy import array, sin, cos, dot, cross, bool_, float64
 from numpy.linalg import norm
+from numpy.typing import NDArray
+
+Vector = NDArray[float64]
 
 
-def vector(vec):
+def vector(vec: Sequence[Union[int, float]]) -> Vector:
+    """Convert a vector `vec` to a NumPy array of floating-point numbers."""
     return array(vec, dtype=float)
 
 
-def normalize(vec):
+def normalize(vec: Vector) -> Vector:
+    """Normalize a vector `vec` to a unit vector."""
     return vec / norm(vec)
 
 
-def same(vec1, vec2, tol):
+def same(vec1: Vector, vec2: Vector, tol: float) -> bool_:
+    """
+    Check if two vectors `vec1` and `vec2` are the same within a tolerance
+    `tol`.
+    """
     return norm(vec1 - vec2) <= tol
 
 
-def parallel(vec1, vec2, tol):
+def parallel(vec1: Vector, vec2: Vector, tol: float) -> bool_:
+    """
+    Check if two vectors `vec1` and `vec2` are parallel within a tolerance
+    `tol`.
+    """
     return norm(cross(vec1, vec2)) <= tol
 
 
-def perpendicular(vec1, vec2, tol):
+def perpendicular(vec1: Vector, vec2: Vector, tol: float) -> bool_:
+    """
+    Check if two vectors `vec1` and `vec2` are perpendicular within a tolerance
+    `tol`.
+    """
     return abs(dot(vec1, vec2)) <= tol
 
 
-def translate(point, translation):
+def translate(point: Vector, translation: Vector) -> Vector:
+    """Translate a point `point` by a translation vector `translation`."""
     return point + translation
 
 
-def invert(point):
-    return - point
+def invert(point: Vector) -> Vector:
+    """Invert a point `point` through the origin."""
+    return -point
 
 
-def translate2(point, normal, coef1, coef2):
+def translate2(
+    point: Vector, normal: Vector, coef1: float, coef2: float
+) -> Vector:
+    """
+    Move a point `point` in a plane containing the point with a normalized
+    normal `normal` to a position represented by the linear combination of the
+    projection of the point position on the plane scaled by `coef1` and its
+    perpendicular in the plane scaled by `coef2`.
+    """
     base = dot(point, normal) * normal
     projection = point - base
     perpendicular_ = cross(normal, projection)
     return base + projection * coef1 + perpendicular_ * coef2
 
 
-def rotate(point, rotation, angle):
+def rotate(point: Vector, rotation: Vector, angle: float) -> Vector:
+    """
+    Rotate a point `point` around a rotation axis with a normalized direction
+    `rotation` by an angle `angle`.
+    """
     return translate2(point, rotation, cos(angle), sin(angle))
 
 
-def reflect(point, reflection):
+def reflect(point: Vector, reflection: Vector) -> Vector:
+    """
+    Reflect a point `point` through a reflection plane with a normalized normal
+    `reflection`.
+    """
     return point - 2 * dot(point, reflection) * reflection
