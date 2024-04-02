3153ae8a7f0eddc3bc88ac9854fe8563a6caf3dd
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2024-04-02 18:42:57 +0200

    Add new constants to const.py

--- symmtools/const.py
+++ symmtools/const.py
@@ -4,8 +4,16 @@ __all__ = [
     "EPS",
     "TOL",
     "PHI",
+    "ORIGIN",
+    "PRIMAX",
+    "SECAX",
+    "TERNAX",
 ]
 
 EPS = 7 / 3 - 4 / 3 - 1
 TOL = 2**4 * EPS
 PHI = (1 + 5**0.5) / 2
+ORIGIN = (0, 0, 0)
+PRIMAX = (0, 0, 1)
+SECAX = (1, 0, 0)
+TERNAX = (0, 1, 0)
