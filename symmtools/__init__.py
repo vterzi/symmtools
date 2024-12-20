dad5569ce0fea680333c46bef2aaa0314d80dfcb
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2024-12-20 21:53:21 +0100

    Improve __init__.py

--- symmtools/__init__.py
+++ symmtools/__init__.py
@@ -1,9 +1,50 @@
-from .const import *
-from .utils import *
-from .transform import *
-from .primitive import *
-from .symmelem import *
-from .quaternion import *
-from .ptgrp import *
-from .chem import *
-from .irrep import *
+__all__ = [
+    "Identity",
+    "Translation",
+    "Rotation",
+    "Reflection",
+    "Rotoreflection",
+    "Point",
+    "Points",
+    "LabeledPoint",
+    "Arrow",
+    "StructPoint",
+    "IdentityElement",
+    "RotationAxis",
+    "ReflectionPlane",
+    "RotoreflectionAxis",
+    "InfRotoreflectionAxis",
+    "AxisRotationAxes",
+    "CenterRotationAxes",
+    "AxisReflectionPlanes",
+    "CenterReflectionPlanes",
+    "CenterRotoreflectionAxes",
+    "Quaternion",
+    "PointGroup",
+    "Plot",
+]
+
+from .transform import (
+    Identity,
+    Translation,
+    Rotation,
+    Reflection,
+    Rotoreflection,
+)
+from .primitive import Point, Points, LabeledPoint, Arrow, StructPoint
+from .symmelem import (
+    IdentityElement,
+    RotationAxis,
+    ReflectionPlane,
+    RotoreflectionAxis,
+    InfRotoreflectionAxis,
+    AxisRotationAxes,
+    CenterRotationAxes,
+    AxisReflectionPlanes,
+    CenterReflectionPlanes,
+    CenterRotoreflectionAxes,
+)
+from .quaternion import Quaternion
+from .ptgrp import PointGroup
+from .misc import Plot
+
