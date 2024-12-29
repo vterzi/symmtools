15f23ee3f71436294cc7bda5cdde6a316a4676a0
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2024-12-29 14:42:09 +0100

    Fix arguments in chem.py

--- symmtools/chem.py
+++ symmtools/chem.py
@@ -12,53 +12,65 @@ __all__ = [
 from .primitive import Arrow, StructPoint
 
 
-def s_orb(pos, coef=1):
+def s_orb(pos, coef=1.0):
     return StructPoint(pos, coef)
 
 
-def px_orb(pos, coef=1):
-    return StructPoint(pos, coef, [Arrow([1, 0, 0], True, False)])
+def px_orb(pos, coef=1.0):
+    return StructPoint(pos, coef, [Arrow((1.0, 0.0, 0.0), True, False)])
 
 
-def py_orb(pos, coef=1):
-    return StructPoint(pos, coef, [Arrow([0, 1, 0], True, False)])
+def py_orb(pos, coef=1.0):
+    return StructPoint(pos, coef, [Arrow((0.0, 1.0, 0.0), True, False)])
 
 
-def pz_orb(pos, coef=1):
-    return StructPoint(pos, coef, [Arrow([0, 0, 1], True, False)])
+def pz_orb(pos, coef=1.0):
+    return StructPoint(pos, coef, [Arrow((0.0, 0.0, 1.0), True, False)])
 
 
-def dx2y2_orb(pos, coef=1):
+def dx2y2_orb(pos, coef=1.0):
     return StructPoint(
         pos,
         coef,
-        [Arrow([1, 0, 0], True, True), Arrow([0, 1, 0], False, False)],
+        [
+            Arrow((1.0, 0.0, 0.0), True, True),
+            Arrow((0.0, 1.0, 0.0), False, False),
+        ],
     )
 
 
-def dz2_orb(pos, coef=1):
-    return StructPoint(pos, coef, [Arrow([0, 0, 1], True, True)])
+def dz2_orb(pos, coef=1.0):
+    return StructPoint(pos, coef, [Arrow((0.0, 0.0, 1.0), True, True)])
 
 
-def dxy_orb(pos, coef=1):
+def dxy_orb(pos, coef=1.0):
     return StructPoint(
         pos,
         coef,
-        [Arrow([1, 1, 0], True, True), Arrow([1, -1, 0], False, False)],
+        [
+            Arrow((1.0, 1.0, 0.0), True, True),
+            Arrow((1.0, -1.0, 0.0), False, False),
+        ],
     )
 
 
-def dyz_orb(pos, coef=1):
+def dyz_orb(pos, coef=1.0):
     return StructPoint(
         pos,
         coef,
-        [Arrow([0, 1, 1], True, True), Arrow([0, 1, -1], False, False)],
+        [
+            Arrow((0.0, 1.0, 1.0), True, True),
+            Arrow((0.0, 1.0, -1.0), False, False),
+        ],
     )
 
 
-def dxz_orb(pos, coef=1):
+def dxz_orb(pos, coef=1.0):
     return StructPoint(
         pos,
         coef,
-        [Arrow([1, 0, 1], True, True), Arrow([-1, 0, 1], False, False)],
+        [
+            Arrow((1.0, 0.0, 1.0), True, True),
+            Arrow((-1.0, 0.0, 1.0), False, False),
+        ],
     )
